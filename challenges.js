cfed62b8fd979cc78041e1f17f9239c6dc238546
yuta <167545138+YASSINEYUTA@users.noreply.github.com>
2026-02-14 17:36:02 +0100

    Add files via upload

---
+++ challenges.js
@@ -0,0 +1,28 @@
+/*
+
+
+// تحدي map filter find - array of object
+let movies = [
+    { id: 1, title: "Interstellar", rating: 8.6, category: "Sci-Fi" },
+    { id: 2, title: "The Dark Knight", rating: 9.0, category: "Action" },
+    { id: 3, title: "Joker", rating: 8.4, category: "Drama" },
+    { id: 4, title: "Inception", rating: 8.8, category: "Sci-Fi" },
+    { id: 5, title: "Avengers", rating: 8.0, category: "Action" }
+];
+
+
+let mov = movies.find(m => m.title === "Joker");
+
+
+let filt = movies.filter(m => m.category === "Action")
+
+let m = movies.map( m => m.title)
+
+console.log(filt)
+console.log(mov)
+console.log(m)
+
+
+
+*/
+
